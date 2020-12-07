0bb84b969a21177942b8710984a0dab86273cc1c
Spencer <58541074+spencer-maaaaan@users.noreply.github.com>
2020-12-06 21:57:12 -0500

    improved regex and added code to construct a list of emoji from any given message

--- bot.py
+++ bot.py
@@ -10,10 +10,14 @@ logging.basicConfig(level=logging.INFO)
 class Client(discord.Client):
     def __init__(self):
         discord.Client.__init__(self)
+        
+        #defining partial (non-custom) emoji list
+        with open("emoji.txt", "r", encoding = "utf-8") as f:
+            self.partials = [line.replace("\n", "") for line in f.readlines()]
 
     async def on_ready(self):
         logging.info("Logged in as {0}".format(self.user))
-
+            
     async def on_message(self, message):
         
         #returning on bot's own messages
@@ -24,17 +28,24 @@ class Client(discord.Client):
         if message.guild == None:
             logging.info("Ignoring PM")
             return
-
-        #if a custom emoji is in the string
-        if re.match(r"<:(.*):([0-9]{18})>", message.content):
-            logging.info("re match")
         
-        #it would be best if I traversed each message word by word
-        #then I could produce a subset of the message's words where every element is an emoji
-        
-        #I still need to account for one case:
-        #   - unicode emoji (some unicode char)
+        #creating sublist for message's emoji
+        emojilist = list()
 
+        #checking for custom/parital emoji in message
+        custom_matches = re.findall(r"<:([0-9a-zA-Z]*):([0-9]{18})>", message.content)
+        
+        if custom_matches:
+            logging.info("custom match")
+            emojilist.extend(custom_matches)
+                
+        for char in message.content:
+            if char in self.partials:
+                logging.info("partial match")
+                emojilist.append(char)
+
+        #for emoji in emojilist, add to guild's emoji counts in dictionary and dump to json
+        
 
 #init client object and starting the bot with secret token
 client = Client()
