0284b6548015e07083fde720d45a62934ad03871
spence <spencer.d.tenney@gmail.com>
2021-01-19 15:20:29 -0500

    adjusted topn to work with new json format

--- topn.py
+++ topn.py
@@ -1,5 +1,6 @@
 """
 takes json and generates a graph of the top n items
+returns the location of the saved graph
 """
 
 import json
@@ -8,15 +9,25 @@ import numpy as np
 
 
 def topn(guildName, guildID, n):
-    #sorting and trimming dict to be top n items
     with open("stats.json", "r") as f:
         guild = json.load(f)[guildID]
     
-    guild = sorted(guild.items(), key=lambda x: x[1])[:-n-1:-1]
+    #summing each user's counts into one dict
+    counts = dict()
+    for user in guild.keys():
+        for emoji in guild[user].keys():
+            if emoji in counts.keys():
+                counts[emoji] += guild[user][emoji]
+
+            else:
+                counts[emoji] = guild[user][emoji]
+
+    #sorting and trimming dict to be top n items
+    counts = sorted(counts.items(), key=lambda x: x[1])[:-n-1:-1]
     
     #defining axes
-    x = [x[0] for x in guild]
-    y = [x[1] for x in guild]
+    x = [x[0] for x in counts]
+    y = [x[1] for x in counts]
     x_pos = np.arange(len(x))
 
     #init graph with colors
