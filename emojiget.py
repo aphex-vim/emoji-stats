99e401ad79b286880485a67ce06779a471ea0667
spence <spencer.d.tenney@gmail.com>
2021-02-02 11:26:58 -0500

    I didn't commit these last night what

--- emojiget.py
+++ emojiget.py
@@ -3,20 +3,21 @@ returns a list of all the emoji in a string
 valid emoji can either be "partial" emojis such as unicode emoji, or custom discord emoji
 """
 import re
+
 import emojis
 
 def emojiget(string):
-    #initializing list and count for partials
+    # initializing list and count for partials
     emojilist = list()
     partial_matches = 0
 
-    #checking for custom discord emojis
+    # checking for custom discord emojis
     custom_matches = re.findall(r"<(a?):([0-9a-zA-Z]*):([0-9]{18})>", string)    
     if custom_matches: 
         custom_matches = [i[1] for i in custom_matches]
         emojilist.extend(custom_matches)
             
-    #checking for partial emojis
+    # checking for partial emojis
     for emoji in emojis.iter(string):
         partial_matches += 1
         emoji = emojis.decode(emoji).replace(":", "")
--- topn.py
+++ topn.py
@@ -4,6 +4,7 @@ returns the location of the saved graph
 """
 
 import json
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -12,7 +13,7 @@ def topn(guildName, guildID, n):
     with open("stats.json", "r") as f:
         guild = json.load(f)[guildID]
     
-    #summing each user's counts into one dict
+    # summing each user's counts into one dict
     counts = dict()
     for user in guild.keys():
         for emoji in guild[user].keys():
@@ -22,15 +23,15 @@ def topn(guildName, guildID, n):
             else:
                 counts[emoji] = guild[user][emoji]
 
-    #sorting and trimming dict to be top n items
+    # sorting and trimming dict to be top n items
     counts = sorted(counts.items(), key=lambda x: x[1])[:-n-1:-1]
     
-    #defining axes
+    # defining axes
     x = [x[0] for x in counts]
     y = [x[1] for x in counts]
     x_pos = np.arange(len(x))
 
-    #init graph with colors
+    # init graph with colors
     params = {
         "ytick.color" : "w",
         "xtick.color" : "w",
@@ -41,28 +42,28 @@ def topn(guildName, guildID, n):
     plt.rcParams.update(params)
     
     ax = plt.axes()
-    ax.set_facecolor("#40444B")
-    plt.bar(x_pos, y, zorder=3, color="#6F85D2")
+    ax.set_facecolor("# 40444B")
+    plt.bar(x_pos, y, zorder=3, color="# 6F85D2")
     plt.title(guildName, color="w")
     plt.xticks(x_pos, x, rotation=90, ma="right")
     
-    #placing value labels on each bar
+    # placing value labels on each bar
     for i in range(len(y)):
         plt.annotate(y[i], xy=(x_pos[i], y[i]), ha="center", va="bottom", color="w")
 
     
-    #setting yticks to reasonable amounts
+    # setting yticks to reasonable amounts
     ymax = max(y)
     plt.yticks(np.arange(0, ymax+(ymax//10), step=ymax//10))
     plt.grid(axis="y", alpha=.5, zorder=0)
     
-    #scaling view to include labels
+    # scaling view to include labels
     plt.tight_layout()
 
-    #saving figure and clearing plot
+    # saving figure and clearing plot
     figName = "./fig/"+guildID+".png"
-    plt.savefig(figName, facecolor="#36393F")
+    plt.savefig(figName, facecolor="# 36393F")
     plt.cla()
 
-    #returning figure location
+    # returning figure location
     return figName
